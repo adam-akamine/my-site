a0cc1e5610651647f712fad3824091d011332fca
kekuso <kekuso808@gmail.com>
2016-10-22 23:46:44 -1000

    Added background image and contact links.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-  res.send('Hello!');
+  res.sendFile(path.join(__dirname + 'index.html'));
 })
 
 var server = app.listen(3000, function () {
